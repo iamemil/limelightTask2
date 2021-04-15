b80cfe14e057806269cfd0462d97d4081a9b1dcc
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2021-04-15 22:23:47 +0200

    changes were made

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 $(window).bind('scroll',function(e){
-    parallaxScroll();
+    parallaxWithScroll();
 });
 
-function parallaxScroll(){
+function parallaxWithScroll(){
    var scrolled = $(window).scrollTop(); 
-   $('.layer-1').css('top',(0-(scrolled*.25))+'px');
-   $('.layer-2').css('top',(0-(scrolled*.5))+'px');
-   $('.layer-3').css('top',(0-(scrolled*.75))+'px');
-   $('.rock-1').css('top',(400-(scrolled*.8))+'px');
-   $('.rock-2').css('top',(200-(scrolled*.6))+'px');
-   $('.rock-3').css('top',(500-(scrolled*.4))+'px'); 
-   $('.rock-4').css('top',(600-(scrolled*.5))+'px'); 
-   $('.rock-5').css('top',(600-(scrolled*.7))+'px'); 
-   $('.rock-6').css('top',(400-(scrolled*.7))+'px'); 
-   $('.rock-7').css('top',(600-(scrolled*.5))+'px'); 
-   $('.rock-8').css('top',(200-(scrolled*.2))+'px');
-   $('.rock-9').css('top',(200-(scrolled*.4))+'px');
+   $('.layer-1').css('top',(0-(scrolled*.3))+'px');
+   $('.layer-2').css('top',(0-(scrolled*.6))+'px');
+   $('.layer-3').css('top',(0-(scrolled*.9))+'px');
+   $('.vangogh-1').css('top',(900-(scrolled*.8))+'px');
+   $('.vangogh-2').css('top',(630-(scrolled*.6))+'px');
+   $('.vangogh-3').css('top',(578-(scrolled*.4))+'px'); 
 }
 
 
